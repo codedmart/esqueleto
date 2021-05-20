c496ce3490bd6298f1a538f8ee4dbcbaf583d03c
Brandon Martin <brandon@codedmart.com>
2021-05-20 01:33:03 +0000

    Trials of not knowing 5

--- src/Database/Esqueleto/Experimental/ToAliasReference.hs
+++ src/Database/Esqueleto/Experimental/ToAliasReference.hs
@@ -18,23 +18,15 @@ class ToAliasReference a where
 
 instance ToAliasReference (SqlExpr (Value a)) where
     toAliasReference aliasSource (ERaw m _)
-      | Just alias <- sqlExprMetaAlias m = pure $ ERaw m{sqlExprMetaIsReference = True} $ \_ info ->
-          (useIdent info aliasSource <> "." <> useIdent info alias, [])
+      | Just _ <- sqlExprMetaAlias m =
+          pure $ ERaw m{sqlExprMetaIsReference = True} $ \_ info ->
+            (useIdent info aliasSource, [])
     toAliasReference _ e = pure e
 
 instance ToAliasReference (SqlExpr (Entity a)) where
     toAliasReference aliasSource (ERaw m _)
-      | Just _ <- sqlExprMetaAlias m, True <- sqlExprMetaIsReference m =
+      | Just _ <- sqlExprMetaAlias m, _ <- sqlExprMetaIsReference m =
           pure $ ERaw m{sqlExprMetaIsReference = True} $ \_ info ->
-            (useIdent info aliasSource, [])
-      | Just _ <- sqlExprMetaAlias m, False <- sqlExprMetaIsReference m =
-          pure $ ERaw m{sqlExprMetaIsReference = True} $ \_ info ->
-            (useIdent info aliasSource, [])
-      | Just _ <- sqlExprMetaAlias m, True <- sqlExprMetaIsReference m =
-          pure $ ERaw m{sqlExprMetaIsReference = False} $ \_ info ->
-            (useIdent info aliasSource, [])
-      | Just _ <- sqlExprMetaAlias m, False <- sqlExprMetaIsReference m =
-          pure $ ERaw m{sqlExprMetaIsReference = False} $ \_ info ->
             (useIdent info aliasSource, [])
     toAliasReference _ e = pure e
 
